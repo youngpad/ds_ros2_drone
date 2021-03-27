8f331e023526f735564037c7c8a8a08247a2996a
martinmaeland <martinmaeland@outlook.com>
2021-03-27 17:57:15 +0100

    now works with gazebo simulation and micrortps bridge

--- ds_ros2_drone/offboard_control.py
+++ ds_ros2_drone/offboard_control.py
@@ -2,10 +2,17 @@ import rclpy
 from rclpy.node import Node
 
 # Import msgs
-from ds_ros2_msgs.msg import OffboardControlMode
-from ds_ros2_msgs.msg import TrajectorySetpoint
-from ds_ros2_msgs.msg import VehicleCommand
-from ds_ros2_msgs.msg import VehicleControlMode # hva brukes denne til?
+# from ds_ros2_msgs.msg import OffboardControlMode
+# from ds_ros2_msgs.msg import TrajectorySetpoint
+# from ds_ros2_msgs.msg import VehicleCommand
+# from ds_ros2_msgs.msg import VehicleControlMode # hva brukes denne til?
+# from ds_ros2_msgs.msg import Timesync
+
+from px4_msgs.msg import OffboardControlMode
+from px4_msgs.msg import TrajectorySetpoint
+from px4_msgs.msg import VehicleCommand
+from px4_msgs.msg import VehicleControlMode # hva brukes denne til?
+from px4_msgs.msg import Timesync
 
 
 # Create drone offboard control class
@@ -15,41 +22,53 @@ class DroneOffboardControl(Node):
         # Init
         super().__init__('drone_offboard_control')
 
-        # Create subscriber
-        self.subscription = self.create_subscription(TrajectorySetpoint, 'use_drone_setpoint', self.update_trajectory_setpoint, 10)
+        # Create subscribers
+        self.trajectory_setpoint_subscriber_ = self.create_subscription(TrajectorySetpoint, 'use_drone_setpoint', self.update_trajectory_setpoint, 10)
+        self.timesync_subscriber_ = self.create_subscription(Timesync, 'Timesync_PubSubTopic', self.update_timestamp, 10)
 
         # Create publishers
         self.offboard_control_mode_publisher_ = self.create_publisher(OffboardControlMode, 'OffboardControlMode_PubSubTopic', 10)
         self.trajectory_setpoint_publisher_ = self.create_publisher(TrajectorySetpoint, 'TrajectorySetpoint_PubSubTopic', 10)
         self.vehicle_command_publisher_ = self.create_publisher(VehicleCommand, 'VehicleCommand_PubSubTopic', 10)
 
-        # Member variables
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.yaw = 0.0
+        # Trajectory setpoint variables
+        self.x_ = 0.0
+        self.y_ = 0.0
+        self.z_ = 0.0
+        self.yaw_ = 0.0
+
+        # Create timestamp msg
+        self.timestamp_ = 0
 
         # Create timer
-        timer_period = 0.1 # 100ms
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.offboard_setpoint_counter_ = 0
+        self.timer_period = 0.1 # 100ms
+        self.timer_ = self.create_timer(self.timer_period, self.timer_callback)
 
-        # Shit happens here
-        self.arm() # Run this once at startup to arm the drone
-        self.publish_offboard_control_mode() # Run this once at startup to tell drone that it will recieve only positional data
-        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 1, 6)
+    # Update the timestamp message
+    def update_timestamp(self, msg):
+        self.timestamp_ = msg.timestamp
 
-    # This function re
+    # This function...
     def timer_callback(self):
+
+        if (self.offboard_setpoint_counter_ == 10):
+            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 1, 6)
+            self.arm() # Run this once at startup to arm the drone
+
         self.publish_offboard_control_mode()
         self.publish_trajectory_setpoint()
+
+        if (self.offboard_setpoint_counter_ < 11):
+            self.offboard_setpoint_counter_ += 1
 
     # Callback method that updates the current trajectory setpoint
     def update_trajectory_setpoint(self, msg):
         # Update member variables with respect to incoming trajectory setpoint
-        msg.x = self.x
-        msg.y = self.y
-        msg.z = self.z
-        msg.yaw = self.yaw
+        self.x_ = msg.x
+        self.y_ = msg.y
+        self.z_ = msg.z
+        self.yaw_ = msg.yaw
 
     # This method arms the drone when called
     def arm(self):
@@ -68,7 +87,7 @@ class DroneOffboardControl(Node):
         # Define OffboardControlMode message type
         msg = OffboardControlMode()
 
-        # msg.timestamp , trenger man denne?
+        msg.timestamp = self.timestamp_
         msg.position = True
         msg.velocity = False
         msg.acceleration = False
@@ -77,27 +96,29 @@ class DroneOffboardControl(Node):
 
         # Publish offboard control mode message
         self.offboard_control_mode_publisher_.publish(msg)
+        #self.get_logger().info('Published offboard control mode')
 
     # Method that publishes trajectory setpoint
     def publish_trajectory_setpoint(self):
         # Define TrajectorySetpoint message type
         msg = TrajectorySetpoint()
 
-        #msg.timestamp , trenger man denne?
-        msg.x = self.x
-        msg.y = self.y
-        msg.z = self.z
-        msg.yaw = self.yaw
+        msg.timestamp = self.timestamp_
+        msg.x = self.x_
+        msg.y = self.y_
+        msg.z = self.z_
+        msg.yaw = self.yaw_
 
         # Publish trajectory setpoint
         self.trajectory_setpoint_publisher_.publish(msg)
+        #self.get_logger().info('Published trajectory setpoints')
 
     # Method that publishes vehicle commands
     def publish_vehicle_command(self, command, param1, param2):
         # Define VehicleCommand message type
         msg = VehicleCommand()
 
-        # msg.timestamp , trenger man denne?
+        msg.timestamp = self.timestamp_
         msg.param1 = float(param1)
         msg.param2 = float(param2)
         msg.command = command
@@ -109,17 +130,17 @@ class DroneOffboardControl(Node):
 
         # Publish vehicle command message
         self.vehicle_command_publisher_.publish(msg)
+        self.get_logger().info('Published vehicle command %s' % (command))
 
 
 def main(args=None):
+
+    print("Starting drone offboard control node..")
+
     rclpy.init(args=args)
-
     drone_offboard_control = DroneOffboardControl()
-
     rclpy.spin(drone_offboard_control)
-
     drone_offboard_control.destroy_node()
-
     rclpy.shutdown()
 
 
